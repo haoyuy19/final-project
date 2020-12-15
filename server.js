a8c240a921b319b83864bd6c1e4ed2051d51ba5a
Haoyu Yang <haoyuy19@gmail.com>
2020-12-15 11:12:16 -0800

    Cors fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+var cors = require('cors');
 const app = express();
 
 // Connect Database
@@ -8,7 +8,7 @@ connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
-
+app.use(cors());
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
